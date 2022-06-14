b4b192ab50daaa54065ec8dab96fec37b1e2045a
András Dobreff <dobreffandras@gmail.com>
2022-06-14 14:21:40 +0200

    Generate input items for blackbox testing
    Creating blackbox tests based on the branches in the code.
    Items are generated and written to the console after applying GildedRose.UpdateQuantity

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,7 +4,7 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        internal IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp;
+
+var quantitites = new[] { -1, 0, 1, 49, 50, 51 };
+var sellins = new[] { -1, 0, 1, 5, 6, 7, 10, 11, 12 };
+var names = new[] {
+    "Anyname",
+    "Aged Brie",
+    "Backstage passes to a TAFKAL80ETC concert",
+    "Sulfuras, Hand of Ragnaros",
+};
+
+
+var items = new List<Item>();
+foreach(var n in names)
+{
+    foreach(var s in sellins)
+    {
+        foreach(var q in quantitites)
+        {
+            items.Add(new Item { Name = n, SellIn = s, Quality = q });
+        }
+    }
+}
+
+var gildedRose = new csharp.GildedRose(items);
+gildedRose.UpdateQuality();
+
+foreach(var item in gildedRose.Items)
+{
+    Console.WriteLine(item);
+}
