49f29d00a8e9f9d9a7feef6ac86b12c905263540
András Dobreff <dobreffandras@gmail.com>
2022-06-14 15:54:27 +0200

    Move quantity check out
    And move SellIn check in
    In the BackstageStrategy

--- GildedRose/BackstageStrategy.cs
+++ GildedRose/BackstageStrategy.cs
@@ -14,17 +14,14 @@ namespace csharp
             {
                 item.Quality++;
 
-                if (item.SellIn < 11)
+                if (item.Quality < 50)
                 {
-                    if (item.Quality < 50)
+                    if (item.SellIn < 11)
                     {
                         item.Quality++;
                     }
-                }
 
-                if (item.SellIn < 6)
-                {
-                    if (item.Quality < 50)
+                    if (item.SellIn < 6)
                     {
                         item.Quality++;
                     }
