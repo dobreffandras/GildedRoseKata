a1cc808f9aa492a512fa3e07c88eaf926abd57b5
András Dobreff <dobreffandras@gmail.com>
2022-06-14 14:45:00 +0200

    Extract magic constants

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,6 +4,10 @@ namespace csharp
 {
     public class GildedRose
     {
+        private const string AgedBire = "Aged Brie";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
         internal IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -14,11 +18,11 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != AgedBire && Items[i].Name != Backstage)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != Sulfuras)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -30,7 +34,7 @@ namespace csharp
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == Backstage)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -51,20 +55,20 @@ namespace csharp
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != Sulfuras)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != AgedBire)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != Backstage)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != Sulfuras)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
